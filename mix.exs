e9873d6ec09074c506c7084f36329a27aff93362
Praveen Perera <ppraveen25@gmail.com>
2020-07-11 23:05:07 -0400

    Add changelog and get ready for publishign on hex

--- mix.exs
+++ mix.exs
@@ -1,28 +1,53 @@
 defmodule GleamCompile.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :gleam_compile,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # hex
+      description:
+        "A tiny hex package makes it easier to use gleam in elixir and phoenix projects ",
+      package: package(),
+
+      # docs
+      source_url: "https://github.com/praveenperera/gleam_compile",
+      homepage_url: "https://github.com/praveenperera/gleam_compile",
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Praveen Perera"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/praveenperera/gleam_compile"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      markdown_processor: ExDoc.Markdown.Earmark,
+      extras: ["README.md", "CHANGELOG.md", "LICENSE.md"],
+      source_ref: "v#{@version}"
+    ]
+  end
+
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
